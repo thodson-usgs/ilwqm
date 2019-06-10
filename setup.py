a61a8c19db8eff30948ddf95598d1764ee12feaa
Timothy Hodson <34148978+thodson-usgs@users.noreply.github.com>
2019-06-10 13:53:26 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md","r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='wry',
+    name='libmod',
     version='0.1',
-    description='Weighted regressions for hydrology.',
-    url='https://github.com/thodson-usgs/wry.git',
+    description='Lasso-Impute-Bootstrap Model.',
+    url='https://github.com/thodson-usgs/libmod.git',
     author='Timothy Hodson',
     author_email='thodson@usgs.gov',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    license='MIT',
+    license='CC0',
     packages=setuptools.find_packages(),
     zip_safe=False
 )
